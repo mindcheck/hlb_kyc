89d32cf76cebc2c89b0aa2f5fb1a9fe2b768b279
kaixuanx3 <kaixuanx3@gmail.com>
2026-01-15 12:21:19 +0800

    Revert "coreLibraryDesugaringEnabled changes"
    This reverts commit c8155316

--- app/src/main/java/com/example/vnkycsdk/MainActivityNew.java
+++ app/src/main/java/com/example/vnkycsdk/MainActivityNew.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.vnkycsdk.databinding.ActivityMainNewBinding;
-
 
 public class MainActivityNew extends AppCompatActivity {
 
